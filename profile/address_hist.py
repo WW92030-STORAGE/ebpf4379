3aebd28bb6df713a1f52646d5f5ca2f400ab7ee4
root <root@node0.wwang26-287907.ldos-ut-pg0.wisc.cloudlab.us>
2026-02-04 16:13:10 -0600

    more timestamps

--- profile/address_hist.py
+++ profile/address_hist.py
@@ -17,6 +17,8 @@ Look into accesses as well?
 from CONSTANTS import BUCKET_ORDER, BUCKET_SHIFT, BUCKET_SIZE, NUM_BUCKETS
 from UTILS import get_benefits
 
+# Actual eBPF system that procure the histogram
+
 prog = """
 #include <uapi/linux/ptrace.h>
 
@@ -44,6 +46,7 @@ b.attach_kprobe(event="handle_mm_fault", fn_name="probe_handle_mm_fault")
 
 print("Tracing... Ctrl-C to stop.")
 
+# Procure and return the histogram
 def print_linear_hist():
     # print("---- Linear VA Fault Histogram ----")
     arr = b.get_table("addr_hist")
@@ -65,6 +68,7 @@ def print_linear_hist():
 
     return res
 
+# Convert the histogram, (start, end, count), into a flat histogram with implicitly defined ranges as indices.
 def get_bucket_info(val):
     res = [0] * NUM_BUCKETS
     for input in val:
@@ -73,24 +77,49 @@ def get_bucket_info(val):
         res[bucket_index] = input[2]
     return res
 
+# Constants and globals
+
+prior_histogram = None
+
+# Runner -- periodically procure a histogram and do updates
 if __name__ == "__main__":
+    """
+
+    Period = seconds per iteration
+    FIXED_VALUES = Whether we fall to the prototype or use the dynamic updates
+    PARALLEL = Whether we parallelize some operations
+    TRADE_VALUE = How much is brought over from the lower-than-avg to the higher-than-avg
+    UB/LB - Attempts to change benefits to outside this range are ignored.
+
+    """
     PERIOD = 8
     FIXED_VALUES = False
     PARALLEL = True
-    NUM_THREADS = 8
+    NUM_THREADS = 16
     TRADE_VALUE = 5000
     UB = 400000 - 20000
     LB = 20000
     while True:
         sleep(PERIOD)
-        val = print_linear_hist()
 
         START = time.perf_counter_ns()
 
+        # Convert the histogram
+        val = print_linear_hist()
         bucket_info = get_bucket_info(val)
+
+        # Count (baseline for averages)
         count = sum(x != 0 for x in bucket_info)
+
+        END = time.perf_counter_ns()
+        ELAPSED_NS = END - START
+        print("XEBPF_COUNT MS:", ELAPSED_NS * 0.000001)
+
         if count:
+            # average number of hits
             mu = sum(bucket_info) / count
+
+            # Fixed benefits (cut and dry) or dynamic updates?
             if FIXED_VALUES:
                 for index in range(len(bucket_info)):
                     if bucket_info[index] == 0:
@@ -114,47 +143,36 @@ if __name__ == "__main__":
                     elif bucket_info[index] < mu:
                         sum_below_avg += deviation
                 
-                def partial_mod(start_val, step):
+                def modify_worker(start_val, step):
                     for index in range(start_val, NUM_BUCKETS, step):
                         deviation = abs(bucket_info[index] - mu)
+                        transfer = (TRADE_VALUE * deviation) // sum_above_avg
                         if bucket_info[index] == 0:
                             continue
+                        SB = time.perf_counter_ns()
                         cur_ben = get_benefits(index)
-                        if bucket_info[index] > mu:
-                            if cur_ben > UB:
-                                continue
-                            cmd = "echo \"%d %d %d\" | sudo tee /proc/increase_benefits" % (index, (TRADE_VALUE * deviation) // sum_above_avg, 1)
-                            exec_(cmd)
+                        EB = time.perf_counter_ns()
+                        print("GET MS: " + str((EB - SB) * 0.000001))
+                        if bucket_info[index] + transfer > mu:
+                            if cur_ben + transfer <= UB:
+                                cmd = "echo \"%d %d %d\" | sudo tee /proc/increase_benefits" % (index, transfer, 1)
+                                exec_(cmd)
                         elif bucket_info[index] < mu:
-                            if cur_ben < UB:
-                                continue
-                            cmd = "echo \"%d %d %d\" | sudo tee /proc/increase_benefits" % (index, (TRADE_VALUE * deviation) // sum_below_avg, 0)
-                            exec_(cmd)
+                            if cur_ben - transfer >= LB:
+                                cmd = "echo \"%d %d %d\" | sudo tee /proc/increase_benefits" % (index, transfer, 0)
+                                exec_(cmd)
+                        EB = time.perf_counter_ns()
+                        print("PUT MS: " + str((EB - SB) * 0.000001))
                 if not PARALLEL:
-                    for index in range(NUM_BUCKETS):
-                        deviation = abs(bucket_info[index] - mu)
-                        if bucket_info[index] == 0:
-                            continue
-                        cur_ben = get_benefits(index)
-                        if bucket_info[index] > mu:
-                            if cur_ben > UB:
-                                continue
-                            cmd = "echo \"%d %d %d\" | sudo tee /proc/increase_benefits" % (index, (TRADE_VALUE * deviation) // sum_above_avg, 1)
-                            exec_(cmd)
-                        elif bucket_info[index] < mu:
-                            if cur_ben < UB:
-                                continue
-                            cmd = "echo \"%d %d %d\" | sudo tee /proc/increase_benefits" % (index, (TRADE_VALUE * deviation) // sum_below_avg, 0)
-                            exec_(cmd)
+                    modify_worker(0, 1)
                 else:
                     with CF.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
                         for i in range(NUM_THREADS):
-                            executor.submit(partial_mod, i, NUM_THREADS)
+                            executor.submit(modify_worker, i, NUM_THREADS)
+        
         END = time.perf_counter_ns()
-
         ELAPSED_NS = END - START
-
-        print("MS:", ELAPSED_NS * 0.000001)
+        print("FULL MS:", ELAPSED_NS * 0.000001)
 
 
 
