6bd0e6b9a0804bd2ee4694a4b5f2cd968afb52ad
root <root@node0.wwang26-297493.ldos-ut-pg0.wisc.cloudlab.us>
2026-03-30 16:05:16 -0500

    no database dir

--- ycsb/install_mongo4.sh
+++ ycsb/install_mongo4.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y docker.io
-sudo systemctl enable --now docker
+PWDIR=$(pwd)
+
+cd
+
+wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
+tar -xvf openssl-1.1.1w.tar.gz
+cd openssl-1.1.1w
+
+./config --prefix=$HOME/openssl-1.1
+make -j$(nproc)
+make install
+
+cd $PWDIR
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.2.24.tgz
+tar -xvf mongodb-linux-x86_64-ubuntu1804-4.2.24.tgz
+cd mongodb-linux-x86_64-ubuntu1804-4.2.24/bin
+
+LD_LIBRARY_PATH=~/openssl-1.1/lib ./mongodb-linux-x86_64-ubuntu1804-4.2.24/bin/mongod --dbpath database
